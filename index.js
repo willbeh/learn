121b0e61a10384c8adc3de9fa6ef888591ef6f7f
William Beh <willbeh@gmail.com>
2025-07-22 14:39:15 +0800

    as

--- index.js
+++ index.js
@@ -16,12 +16,7 @@ const nameList = [
 ]
 // routes
 app.get("/", (req, res) => {
-  res.send(`Hello ${nameList[Math.floor(Math.random() * nameList.length)]}`);
-});
-
-app.get("/:id", (req, res) => {
-  const id = req.params.id;
-  res.send(`Hello ${nameList[id]}`);
+  res.send(`Hello ${nameList.map((name, index) => `${index}: ${name}`).join(", ")}`);
 });
 
 // TASK: Add name to the nameList
@@ -29,6 +24,11 @@ app.get("/:id", (req, res) => {
 // test using CURL: 
 // curl http://localhost:3000/add-name -X POST -d "name=John"
 //
+app.post("/add-name", (req, res) => {
+  const name = req.body.name;
+  nameList.push(name);
+  res.send(`Added ${name} to the nameList`);
+});
 
 // TASK Update name in the namelist based on the id
 // path: /update-name/:id
@@ -41,6 +41,11 @@ app.get("/:id", (req, res) => {
 // test using CURL: 
 // curl http://localhost:3000/delete-name/1 -X DELETE
 //
+
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`Hello ${nameList[id]} aaa`);
+});
 
 // listen
 app.listen(3000, () => {
